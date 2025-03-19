e796cd87e75638462fb8a8a407f0371ba093a857
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2025-03-19 23:23:08 +0200

    add hero and ReviewBtn

---
+++ src/components/ReviewBtn/ReviewBtn.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import styles from './ReviewBtn.module.scss';
+
+const ReviewBtn = ({ href, id }) => {
+  return (
+    <Link href={href} className={styles.btn} id={id}>
+      Переглянути
+    </Link>
+  );
+};
+
+export default ReviewBtn;
