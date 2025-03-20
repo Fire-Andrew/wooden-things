ce35ae0f2814af5f6316fd3f4620224cc834057a
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2025-03-21 00:19:17 +0200

    add HomeIgnitersSection

--- src/sections/homeOakParquetSection/HomeOakParquetSection.jsx
+++ src/sections/homeOakParquetSection/HomeOakParquetSection.jsx
@@ -9,33 +9,35 @@ const HomeOakParquetSection = () => {
   const { isMobile } = useWindowResize();
   return (
     <section className={`section`}>
-      <h2 className={`sectionTitle ${styles.title}`}>Паркет дубовий</h2>
-      <div className={styles.content}>
-        {isMobile ? (
-          <p className={styles.text}>
-            Lorem ipsum dolor sit amet consectetur. Augue quis aliquet justo
-            odio pretium vivamus arcu lobortis.
-          </p>
-        ) : (
-          <p className={styles.text}>
+      <div className="container">
+        <h2 className={`sectionTitle ${styles.title}`}>Паркет дубовий</h2>
+        <div className={styles.content}>
+          {isMobile ? (
+            <p className={styles.text}>
+              Lorem ipsum dolor sit amet consectetur. Augue quis aliquet justo
+              odio pretium vivamus arcu lobortis.
+            </p>
+          ) : (
+            <p className={styles.text}>
+              Lorem ipsum dolor sit amet consectetur. Augue quis aliquet justo
+              odio pretium vivamus arcu lobortis. Netus faucibus curabitur
+              tellus diam odio pulvinar eu. Lacus lectus ultrices faucibus nec
+              lectus odio dictum. Non pretium nunc felis ultricies cras.
+            </p>
+          )}
+          <figure className={styles.imgContainer}>
+            <Image src="/png/OakParquet.png" alt="Паркет дубовий" fill={true} />
+          </figure>
+        </div>
+        {isMobile && (
+          <p className={styles.textBottom}>
             Lorem ipsum dolor sit amet consectetur. Augue quis aliquet justo
             odio pretium vivamus arcu lobortis. Netus faucibus curabitur tellus
             diam odio pulvinar eu. Lacus lectus ultrices faucibus nec lectus
             odio dictum. Non pretium nunc felis ultricies cras.
           </p>
         )}
-        <figure className={styles.imgContainer}>
-          <Image src="/png/OakParquet.png" alt="Паркет дубовий" fill={true} />
-        </figure>
       </div>
-      {isMobile && (
-        <p className={styles.textBottom}>
-          Lorem ipsum dolor sit amet consectetur. Augue quis aliquet justo odio
-          pretium vivamus arcu lobortis. Netus faucibus curabitur tellus diam
-          odio pulvinar eu. Lacus lectus ultrices faucibus nec lectus odio
-          dictum. Non pretium nunc felis ultricies cras.
-        </p>
-      )}
     </section>
   );
 };
