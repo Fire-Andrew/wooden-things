b865e49f1ac0f944af988faa1f00649e73f6493f
Nataliia <natalabojko@gmail.com>
2025-03-21 00:14:58 +0200

    startFooter

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import styles from "./Footer.module.scss";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className={`section ${styles.footer}`}>
+      <div className="container"></div>
+      <p className={styles.allRights}>
+        © All rights reserved by{" "}
+        <a href="https://www.webevery.dev/">Webevery.dev</a> 2025
+      </p>
+      <div className="container"></div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
