3822a05dc763555b4277d0a13c00b469d2ab0219
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2025-03-21 23:34:26 +0200

    translate home page, add sizes in image and change OakParquet

--- src/sections/homeOakParquetSection/HomeOakParquetSection.jsx
+++ src/sections/homeOakParquetSection/HomeOakParquetSection.jsx
@@ -1,42 +1,38 @@
-'use client';
-
 import React from 'react';
 import styles from './HomeOakParquetSection.module.scss';
 import Image from 'next/image';
-import { useWindowResize } from '@/hooks/windowResize';
 
 const HomeOakParquetSection = () => {
-  const { isMobile } = useWindowResize();
   return (
     <section className={`section`}>
       <div className="container">
-        <h2 className={`sectionTitle ${styles.title}`}>Паркет дубовий</h2>
+        <h2 className={`sectionTitle ${styles.title}`}>Oak parquet</h2>
         <div className={styles.content}>
-          {isMobile ? (
-            <p className={styles.text}>
-              Lorem ipsum dolor sit amet consectetur. Augue quis aliquet justo
-              odio pretium vivamus arcu lobortis.
-            </p>
-          ) : (
-            <p className={styles.text}>
-              Lorem ipsum dolor sit amet consectetur. Augue quis aliquet justo
-              odio pretium vivamus arcu lobortis. Netus faucibus curabitur
-              tellus diam odio pulvinar eu. Lacus lectus ultrices faucibus nec
-              lectus odio dictum. Non pretium nunc felis ultricies cras.
-            </p>
-          )}
+          <ul className={styles.textContainer}>
+            <li>Sustainability & Growth</li>
+            <li>
+              We source wood from FSC-certified forests, ensuring eco-friendly
+              production and responsible forestry.
+            </li>
+            <li>To enhance global logistics, we are:</li>
+            <li>
+              Establishing an EU Company – Improving customer service and
+              operations within Europe.
+            </li>
+            <li>
+              Expanding EU Distribution – Setting up a warehouse in Poland for
+              faster, cost-effective deliveries.
+            </li>
+          </ul>
           <figure className={styles.imgContainer}>
-            <Image src="/png/OakParquet.png" alt="Паркет дубовий" fill={true} />
+            <Image
+              src="/png/OakParquet.png"
+              alt="Паркет дубовий"
+              fill={true}
+              sizes="(max-width: 744px) 50%"
+            />
           </figure>
         </div>
-        {isMobile && (
-          <p className={styles.textBottom}>
-            Lorem ipsum dolor sit amet consectetur. Augue quis aliquet justo
-            odio pretium vivamus arcu lobortis. Netus faucibus curabitur tellus
-            diam odio pulvinar eu. Lacus lectus ultrices faucibus nec lectus
-            odio dictum. Non pretium nunc felis ultricies cras.
-          </p>
-        )}
       </div>
     </section>
   );
