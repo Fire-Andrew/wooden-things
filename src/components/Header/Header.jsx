562cc79bbdd5e85421ac3dfdb854cc06b3048b54
Nataliia <natalabojko@gmail.com>
2025-03-22 17:03:24 +0200

    activeLinks

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,20 +9,23 @@ import Navigation from "../Navigation/Navigation";
 import MobileMenu from "./MobileMenu/MobileMenu";
 import { useCloseMenu } from "@/hooks/closeMenu";
 import Languages from "../Languages/Languages";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
   const { isMobile, isLaptop } = useWindowResize();
 
   const { handleClick } = useCloseMenu();
 
-  const [showLang, setShowLang] = useState(false);
+  // const [showLang, setShowLang] = useState(false);
 
   return (
     <header className={styles.header}>
       <div className={`container ${styles.container}`}>
         <Logo onClick={handleClick} className={styles.logo} />
 
-        {isLaptop && <Navigation className={styles.nav} />}
+        {isLaptop && (
+          <Navigation className={styles.nav} onClick={handleClick} />
+        )}
 
         {!isMobile && <Languages className={styles.langSwitch} />}
 
