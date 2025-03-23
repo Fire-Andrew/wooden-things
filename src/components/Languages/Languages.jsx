c3ef53028560f601b729e729fa93a75ded8845d4
Maksym Padalka <sheihmaks@gmail.com>
2025-03-23 01:22:49 +0100

    +

--- src/components/Languages/Languages.jsx
+++ src/components/Languages/Languages.jsx
@@ -2,13 +2,14 @@
 
 import React from "react";
 import { v4 } from 'uuid';
+import styles from "./LangSwitch.module.scss";
 import { languagesData } from "@/data/languagesData";
 
 const Languages = ({ className, changeLanguage, currentLanguage }) => {
   const languages = Object.values(languagesData);
 
   return (
-    <ul className={className}>
+    <ul className={styles.langSwitch}>
       {languages.map((el) => {
       return <li key={v4()} onClick={()=>changeLanguage(el)}>{el}</li>;
       })}
