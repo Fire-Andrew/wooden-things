9477d00507dcca0453da36ca6bbc2a50d2f20904
Maksimchik36 <Peltek1985@gmail.com>
2025-03-24 13:05:13 +0200

    changed igniters to fire starters

---
+++ src/app/(pages)/fire-starters/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import FireStartersSection from '@/sections/fireStartersSection/FireStartersSection';
+
+
+const page = () => {
+  
+  return (
+    <>
+      <FireStartersSection />
+    </>
+  )
+}
+
+
+export default page;
