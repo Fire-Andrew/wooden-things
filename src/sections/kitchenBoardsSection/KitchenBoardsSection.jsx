73a60b7a4b0fce6d6a834c649e0b1587cfcdfabb
Maksimchik36 <Peltek1985@gmail.com>
2025-03-24 21:03:05 +0200

    renamed more detail btn, priority and loadingfixing

--- src/sections/kitchenBoardsSection/KitchenBoardsSection.jsx
+++ src/sections/kitchenBoardsSection/KitchenBoardsSection.jsx
@@ -25,6 +25,7 @@ const KitchenBoardsSection = () => {
                     sizes="(max-width: 743px) 95vw, (max-width: 1279px) 45vw, 580px"
                     width={703}
                     height={468}
+                    loading={product.loading || "lazy"}
                     priority={product.priority || false}
                   />
               </div>      
@@ -42,60 +43,3 @@ const KitchenBoardsSection = () => {
 
 
 export default KitchenBoardsSection;
-
-
-
-
-
-
-
-
-
-
-// "use client";
-// import React from 'react';
-// import Image from 'next/image';
-// import ReviewBtn from '@/components/ReviewBtn/ReviewBtn';
-// import { productsData } from '@/data/productsData';
-// import styles from './KitchenBoardsSection.module.scss';
-
-
-// const KitchenBoardsSection = () => {
- 
-
-//   return (
-//     <section className={`section ${styles.section}`}>
-//       <div className='container'>
-//         <h2 className={`sectionTitle ${styles.title}`}>Kitchen Chopping Boards</h2>
-
-//         <ul>
-//           {productsData.map((product) =>
-//             <li key={product.slug} className={styles.productCard}>
-//               <div className={styles.imgAndSizesWrap}>
-//                     <div className={styles.imgWrapper}>
-//                       <Image
-//                         className={styles.img}
-//                         src={product.images[0].src}
-//                         alt={product.images[0].src}
-//                         sizes="(max-width: 743px) 100vw, (max-width: 1279px) 50vw,   580px"
-//                         width={703}
-//                         height={468}
-//                       />
-//                     </div>
-//                     <p className={styles.sizes}>{product.sizes}</p>
-//               </div>
-
-//               <div className={styles.infoWrap}>
-//                   <p className={styles.description}>{product.descriptionEn}</p>        
-//                   <p className={styles.material}>Material: {product.materialEn}</p>
-//                   <ReviewBtn href={`/kitchen-boards/${product.slug}`} id={styles.btn}/>
-//               </div>                   
-//           </li>)}
-//         </ul>
-//       </div>
-//     </section>
-//   )
-// }
-
-
-// export default KitchenBoardsSection
