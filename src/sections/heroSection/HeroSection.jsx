b55f40d3b99fd406fbb31a0953149e1d190c57b6
Maksym Padalka <sheihmaks@gmail.com>
2025-03-26 09:42:22 +0100

    +

--- src/sections/heroSection/HeroSection.jsx
+++ src/sections/heroSection/HeroSection.jsx
@@ -1,83 +1,12 @@
-'use client';
-
-import Image from 'next/image';
-import { useWindowResize } from '@/hooks/windowResize';
-import ReviewBtn from '@/components/ReviewBtn/ReviewBtn';
-import styles from './HeroSection.module.scss';
-
-const HeroSection = () => {
-  const { isTablet, isMobile } = useWindowResize();
-  return (
-    <section className={styles.hero} id="hero">
-      <div className={styles.boardContainer}>
-        <h1 className={styles.titleHidden}>Gallen 1 LLC</h1>
-        <figure className={styles.imgBoard}>
-          <Image
-            src="/webp/board-home.webp"
-            alt="board"
-            fill={true}
-            sizes="(max-width: 743px) 154px, (max-width: 1279px) 286px, 650px"
-            loading='eager'
-            priority
-          />
-        </figure>
-        <figure className={styles.imgArrow}>
-          <Image
-            src={
-              isMobile
-                ? '/webp/arrow-home-mobile.webp'
-                : isTablet
-                ? '/webp/arrow-home-tablet.webp'
-                : '/webp/arrow-home-desk.webp'
-            }
-            alt="arrow"
-            fill={true}
-            sizes="(max-width: 743px) 132px, (max-width: 806px) 258px, (max-width: 919px) 228px, (max-width: 1279px) 188px, 330px"
-            loading='eager'
-            priority
-          />
-        </figure>
-        <h2 className={`sectionTitle ${styles.title}`}>Boards</h2>
-        <ul className={styles.textContainer}>
-          <li>
-            With a highly competitive market, we set ourselves apart by
-            delivering exceptional quality.
-          </li>
-          <li>
-            This commitment has earned us a strong global presence, with 100% of
-            our products exported to the United States, Great Britain, Germany,
-            the Netherlands, Denmark, and France.
-          </li>
-          <li>
-            Using state-of-the-art machinery from leading global manufacturers,
-            we ensure precision, efficiency, and uncompromising craftsmanship in
-            every product.
-          </li>
-        </ul>
-        <ReviewBtn href="kitchen-boards" id={styles.btn} />
-      </div>
-    </section>
-  );
-};
-
-export default HeroSection;
-
 // 'use client';
 
 // import Image from 'next/image';
 // import { useWindowResize } from '@/hooks/windowResize';
 // import ReviewBtn from '@/components/ReviewBtn/ReviewBtn';
 // import styles from './HeroSection.module.scss';
-// import { useTranslation } from 'react-i18next';
-// import { useEffect, useState } from 'react';
 
 // const HeroSection = () => {
 //   const { isTablet, isMobile } = useWindowResize();
-//   const[isLoad,setIsLoad]=useState(true)
-//   const {t}=useTranslation();
-
-//   useEffect(()=>{ setIsLoad(false)},[])
-
 //   return (
 //     <section className={styles.hero} id="hero">
 //       <div className={styles.boardContainer}>
@@ -108,18 +37,23 @@ export default HeroSection;
 //             priority
 //           />
 //         </figure>
-//         <h2 className={`sectionTitle ${styles.title}`}>{!isLoad && t('Hero.Title')}</h2>
-//         {!isLoad && <ul className={styles.textContainer}>
+//         <h2 className={`sectionTitle ${styles.title}`}>Boards</h2>
+//         <ul className={styles.textContainer}>
 //           <li>
-//           {t('Hero.Text1')}
+//             With a highly competitive market, we set ourselves apart by
+//             delivering exceptional quality.
 //           </li>
 //           <li>
-//           {t('Hero.Text2')}
+//             This commitment has earned us a strong global presence, with 100% of
+//             our products exported to the United States, Great Britain, Germany,
+//             the Netherlands, Denmark, and France.
 //           </li>
 //           <li>
-//           {t('Hero.Text3')}
+//             Using state-of-the-art machinery from leading global manufacturers,
+//             we ensure precision, efficiency, and uncompromising craftsmanship in
+//             every product.
 //           </li>
-//         </ul>}
+//         </ul>
 //         <ReviewBtn href="kitchen-boards" id={styles.btn} />
 //       </div>
 //     </section>
@@ -127,3 +61,69 @@ export default HeroSection;
 // };
 
 // export default HeroSection;
+
+'use client';
+
+import Image from 'next/image';
+import { useWindowResize } from '@/hooks/windowResize';
+import ReviewBtn from '@/components/ReviewBtn/ReviewBtn';
+import styles from './HeroSection.module.scss';
+import { useTranslation } from 'react-i18next';
+import { useEffect, useState } from 'react';
+
+const HeroSection = () => {
+  const { isTablet, isMobile } = useWindowResize();
+  const[isLoad,setIsLoad]=useState(true)
+  const {t}=useTranslation();
+
+  useEffect(()=>{ setIsLoad(false)},[])
+
+  return (
+    <section className={styles.hero} id="hero">
+      <div className={styles.boardContainer}>
+        <h1 className={styles.titleHidden}>Gallen 1 LLC</h1>
+        <figure className={styles.imgBoard}>
+          <Image
+            src="/webp/board-home.webp"
+            alt="board"
+            fill={true}
+            sizes="(max-width: 743px) 154px, (max-width: 1279px) 286px, 650px"
+            loading='eager'
+            priority
+          />
+        </figure>
+        <figure className={styles.imgArrow}>
+          <Image
+            src={
+              isMobile
+                ? '/webp/arrow-home-mobile.webp'
+                : isTablet
+                ? '/webp/arrow-home-tablet.webp'
+                : '/webp/arrow-home-desk.webp'
+            }
+            alt="arrow"
+            fill={true}
+            sizes="(max-width: 743px) 132px, (max-width: 806px) 258px, (max-width: 919px) 228px, (max-width: 1279px) 188px, 330px"
+            loading='eager'
+            priority
+          />
+        </figure>
+        <h2 className={`sectionTitle ${styles.title}`}>{!isLoad && t('Hero.Title')}</h2>
+        {!isLoad && <ul className={styles.textContainer}>
+          <li>
+          {t('Hero.Text1')}
+          </li>
+          <li>
+          {t('Hero.Text2')}
+          </li>
+          <li>
+          {t('Hero.Text3')}
+          </li>
+        </ul>}
+        <ReviewBtn href="kitchen-boards" id={styles.btn} />
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
--- src/sections/kitchenBoardsSection/KitchenBoardsSection.jsx
+++ src/sections/kitchenBoardsSection/KitchenBoardsSection.jsx
@@ -1,53 +1,6 @@
-"use client";
-import React from 'react';
-import Image from 'next/image';
-import ReviewBtn from '@/components/ReviewBtn/ReviewBtn';
-import { productsData } from '@/data/productsData';
-import styles from './KitchenBoardsSection.module.scss';
-
-
-const KitchenBoardsSection = () => {
- 
-
-  return (
-    <section className={`section ${styles.section}`}>
-      <div className='container'>
-        <h2 className={`sectionTitle ${styles.title}`}>Kitchen Chopping Boards</h2>
-
-        <ul>
-          {productsData.map((product) =>
-            <li key={product.slug} className={styles.productCard}>
-              <div className={styles.imgWrapper}>
-                  <Image
-                    className={styles.img}
-                    src={product.images[0].src}
-                    alt={product.images[0].src}                    
-                    sizes="(max-width: 743px) 95vw, (max-width: 1279px) 45vw, 580px"
-                    width={703}
-                    height={468}
-                    priority={product.priority || false}
-                  />
-              </div>      
-
-              <div className={styles.infoWrap}>
-                  <p className={styles.description}>{product.descriptionEn}</p>        
-                  <ReviewBtn href={`/kitchen-boards/${product.slug}`}/>
-              </div>                   
-          </li>)}
-        </ul>
-      </div>
-    </section>
-  )
-}
-
-
-export default KitchenBoardsSection;
-
 // "use client";
 // import React from 'react';
 // import Image from 'next/image';
-// import { useState, useEffect } from 'react';
-// import { useTranslation } from 'react-i18next';
 // import ReviewBtn from '@/components/ReviewBtn/ReviewBtn';
 // import { productsData } from '@/data/productsData';
 // import styles from './KitchenBoardsSection.module.scss';
@@ -55,17 +8,11 @@ export default KitchenBoardsSection;
 
 // const KitchenBoardsSection = () => {
  
-//   const[isLoad,setIsLoad]=useState(true)
-
-//   const{t}=useTranslation()
-
-//   useEffect(()=>{setIsLoad(false)},[])
-
 
 //   return (
 //     <section className={`section ${styles.section}`}>
 //       <div className='container'>
-//         <h2 className={`sectionTitle ${styles.title}`}>{!isLoad && t("KitchenBoardsSection.Title")}</h2>
+//         <h2 className={`sectionTitle ${styles.title}`}>Kitchen Chopping Boards</h2>
 
 //         <ul>
 //           {productsData.map((product) =>
@@ -95,3 +42,56 @@ export default KitchenBoardsSection;
 
 
 // export default KitchenBoardsSection;
+
+"use client";
+import React from 'react';
+import Image from 'next/image';
+import { useState, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import ReviewBtn from '@/components/ReviewBtn/ReviewBtn';
+import { productsData } from '@/data/productsData';
+import styles from './KitchenBoardsSection.module.scss';
+
+
+const KitchenBoardsSection = () => {
+ 
+  const[isLoad,setIsLoad]=useState(true)
+
+  const{t}=useTranslation()
+
+  useEffect(()=>{setIsLoad(false)},[])
+
+
+  return (
+    <section className={`section ${styles.section}`}>
+      <div className='container'>
+        <h2 className={`sectionTitle ${styles.title}`}>{!isLoad && t("KitchenBoardsSection.Title")}</h2>
+
+        <ul>
+          {productsData.map((product) =>
+            <li key={product.slug} className={styles.productCard}>
+              <div className={styles.imgWrapper}>
+                  <Image
+                    className={styles.img}
+                    src={product.images[0].src}
+                    alt={product.images[0].src}                    
+                    sizes="(max-width: 743px) 95vw, (max-width: 1279px) 45vw, 580px"
+                    width={703}
+                    height={468}
+                    priority={product.priority || false}
+                  />
+              </div>      
+
+              <div className={styles.infoWrap}>
+                  <p className={styles.description}>{product.descriptionEn}</p>        
+                  <ReviewBtn href={`/kitchen-boards/${product.slug}`}/>
+              </div>                   
+          </li>)}
+        </ul>
+      </div>
+    </section>
+  )
+}
+
+
+export default KitchenBoardsSection;
