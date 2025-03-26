d39015dcc57dfb5d1237aee1ba45e7098fd38409
Maksym Padalka <sheihmaks@gmail.com>
2025-03-26 14:18:52 +0100

    +

--- src/sections/homeFireStartersSection/HomeFireStartersSection.jsx
+++ src/sections/homeFireStartersSection/HomeFireStartersSection.jsx
@@ -1,30 +1,3 @@
-// import React from 'react';
-// import ReviewBtn from '@/components/ReviewBtn/ReviewBtn';
-// import styles from './HomeFireStartersSection.module.scss';
-
-// const HomeFireStartersSection = () => {
-//   return (
-//     <section className={`section ${styles.container}`}>
-//       <div className={` container ${styles.ignitersContent}`}>
-//         <h2 className={`sectionTitle ${styles.title}`}>Fire Starters</h2>
-//         <ul className={styles.content}>
-//           <li>
-//             Made from dry wood shavings and purified wax, our chemical-free,
-//             eco-friendly fire starters provide a strong, reliable ignition in
-//             any weather.
-//           </li>
-//           <li>
-//             We hold patents and utility model rights, securing exclusive,
-//             high-performance technology.
-//           </li>
-//         </ul>
-//         <ReviewBtn href="fire-starters" id={styles.btn} />
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default HomeFireStartersSection;
 
 "use client"
 import React from 'react';
--- src/sections/homeOakParquetSection/HomeOakParquetSection.jsx
+++ src/sections/homeOakParquetSection/HomeOakParquetSection.jsx
@@ -1,35 +1,3 @@
-// import React from 'react';
-// import styles from './HomeOakParquetSection.module.scss';
-// import Image from 'next/image';
-
-// const HomeOakParquetSection = () => {
-//   return (
-//     <section className={`section`}>
-//       <div className="container">
-//         <h2 className={`sectionTitle ${styles.title}`}>Oak parquet</h2>
-//         <div className={styles.content}>
-//           <p className={styles.textContainer}>
-//             Crafted from high-quality oak, our elegant and durable parquet
-//             flooring combines timeless beauty, strength, and precision—perfect
-//             for homes and commercial spaces.
-//           </p>
-//           <figure className={styles.imgContainer}>
-//             <Image
-//               src="/webp/OakParquet.webp"
-//               alt="Паркет дубовий"
-//               fill={true}
-//               sizes="(max-width: 743px) 95vw, (max-width: 1279px) 45vw, 580px"
-//               loading="lazy"
-//             />
-//           </figure>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default HomeOakParquetSection;
-
 "use client"
 import React from 'react';
 import styles from './HomeOakParquetSection.module.scss';
