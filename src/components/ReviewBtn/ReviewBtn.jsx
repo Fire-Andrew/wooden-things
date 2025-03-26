064d9f030444c198242b602603ba00b1fb94831d
Maksimchik36 <Peltek1985@gmail.com>
2025-03-26 16:58:29 +0200

    fixed some translator bugs

--- src/components/ReviewBtn/ReviewBtn.jsx
+++ src/components/ReviewBtn/ReviewBtn.jsx
@@ -1,11 +1,19 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
 import Link from 'next/link';
 import styles from './ReviewBtn.module.scss';
 
 
 const ReviewBtn = ({ href, id }) => {
+  const { t } = useTranslation();
+  const [isLoading, setIsLoading] = useState(true); 
+  useEffect(() => setIsLoading(false), []);
+  
+  
   return (
     <Link href={href} className={styles.btn} id={id}>
-      Learn more
+      {!isLoading && t('Buttons.More')}
     </Link>
   );
 };
