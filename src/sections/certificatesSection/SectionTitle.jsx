5540465bbb61dcd9af85dd59ca65ff1fab593fe9
Denys <denys.khmyra.work@gmail.com>
2025-03-27 13:17:05 +0200

    add mobile title

--- src/sections/certificatesSection/SectionTitle.jsx
+++ src/sections/certificatesSection/SectionTitle.jsx
@@ -1,23 +1,22 @@
 "use client";
+
 import { useEffect, useState } from "react";
-import { useTranslation } from 'react-i18next';
-import { useWindowResize } from "@/hooks/windowResize";
+import { useTranslation } from "react-i18next";
 import styles from "./CertificatesSection.module.scss";
 
-
 const SectionTitle = () => {
-    const { isLaptop } = useWindowResize(); 
-    
     const { t } = useTranslation();
     const [isLoading, setIsLoading] = useState(true);
-    useEffect(()=>{ setIsLoading(false)},[]);
-    
+
+    useEffect(() => {
+        setIsLoading(false);
+    }, []);
 
     return (
         <>
-            {isLaptop && (
-                <h2 className={`sectionTitle ${styles.title}`}>{!isLoading && t('CertificatesPage.Title') }</h2>
-            )}
+            <h2 className={`sectionTitle ${styles.title}`}>
+                {!isLoading && t("CertificatesPage.Title")}
+            </h2>
         </>
     );
 };
